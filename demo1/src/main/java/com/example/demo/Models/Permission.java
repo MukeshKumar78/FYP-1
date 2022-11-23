534c6ad36dfb84bc2ce9f2be6eb6e2bb3baaf58a
Mukesh Kumar <mukeshkumr.78@gmail.com>
2022-11-23 17:35:59 +0500

    Added Role Class

--- demo1/src/main/java/com/example/demo/Models/Permission.java
+++ demo1/src/main/java/com/example/demo/Models/Permission.java
@@ -1,5 +1,31 @@
 package com.example.demo.Models;
 
-public class Permission {
+import javax.persistence.*;
 
+@Entity
+public class Permission {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String code;
+
+    @Column
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "role_ID")
+    private Role role;
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
