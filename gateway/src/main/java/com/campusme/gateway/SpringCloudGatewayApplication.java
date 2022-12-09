df29951f115fe36c6cf04c38bddcdf9d6fbed7f0
mohsin <mohsinshk4026@gmail.com>
2022-12-10 02:16:24 +0500

    jwt external and internal auth

--- gateway/src/main/java/com/campusme/gateway/SpringCloudGatewayApplication.java
+++ gateway/src/main/java/com/campusme/gateway/SpringCloudGatewayApplication.java
@@ -2,8 +2,12 @@ package com.campusme.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.campusme.gateway.config.JwtConfig;
 
 @SpringBootApplication
+@EnableConfigurationProperties(JwtConfig.class)
 public class SpringCloudGatewayApplication {
 
     public static void main(String[] args) {
