b8af0f3ff3156f01c1271e5dc4c75d59d9cc5866
roomiOwO <rooman.asifkhan@gmail.com>
2023-01-05 23:04:19 +0500

    details->draw list->map +settings page update

--- ui/packages/app/features/home/screen.tsx
+++ ui/packages/app/features/home/screen.tsx
@@ -1,18 +1,16 @@
 import { View, H1 } from 'dripsy'
-import { EventList } from '../event/event-list'
+import { EventMap } from '../event/event-map'
 
 export function HomeScreen() {
-
   return (
     <View
-      sx={{ 
-        backgroundColor: "$background", 
-        flex: 1, 
-        alignItems: 'center', 
-       }}
+      sx={{
+        backgroundColor: '$background',
+        flex: 1,
+        alignItems: 'center',
+      }}
     >
-      <EventList/>
-
+      <EventMap />
     </View>
   )
 }
---
+++ ui/packages/app/features/society/society-map.tsx
@@ -0,0 +1,6 @@
+import { ScaledImage } from 'app/components/ScaledImage'
+import { View as DView } from 'dripsy'
+import { Image, StyleSheet } from 'react-native'
+import { ButtonWithChildren } from '../../components/Button'
+import Text from '../../components/Text'
+import { Ionicons } from '@expo/vector-icons'
