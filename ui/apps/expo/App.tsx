c497d0ce07388f212b54e423847b7f1d327411e4
Mohsin <mohsinshk4026@gmail.com>
2023-04-14 18:48:07 +0500

    add code for handling notification interaction

--- ui/apps/expo/App.tsx
+++ ui/apps/expo/App.tsx
@@ -1,25 +1,45 @@
 import { NativeNavigation } from 'app/navigation/native'
 import { Provider } from 'app/provider'
 import { useEffect } from 'react';
-import { StatusBar, StyleSheet } from 'react-native'
+import { StatusBar } from 'react-native'
 import { useFonts } from 'expo-font';
 import * as Notifications from 'expo-notifications';
+import * as SQLite from 'expo-sqlite';
+import { Notification } from 'expo-notifications';
 
-Notifications.setNotificationHandler({
-  handleNotification: async () => ({
-    shouldShowAlert: true,
-    shouldPlaySound: false,
-    shouldSetBadge: false,
-  }),
-});
+const db = SQLite.openDatabase('campusme.db');
+
+Notifications.setNotificationHandler(null);
+
+function saveNotification(notification: Notification) {
+  db.exec([{
+    sql: "INSERT INTO notifications (title, body) values (?, ?);",
+    args: [notification.request.content.title, notification.request.content.body]
+  }],
+    false,
+    () => console.log('add to db', notification.request.content)
+  )
+}
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     'Raleway': require('./assets/fonts/Raleway-Regular.ttf'),
   });
 
+  const lastNotificationResponse = Notifications.useLastNotificationResponse();
+
+  console.log(lastNotificationResponse)
+
   useEffect(() => {
+    db.exec([{ sql: 'CREATE TABLE IF NOT EXISTS notifications (title TEXT, body TEXT)', args: [] }], false, () => {
+
+    })
     StatusBar.setBarStyle('dark-content', false)
+
+
+    /* Not working on development builds */
+    // Notifications.addNotificationReceivedListener(saveNotification)
+    // Notifications.addNotificationResponseReceivedListener(r => saveNotification(r.notification))
   }, [])
 
 
