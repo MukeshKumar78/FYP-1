220a8949ad4c2870ac338f69217615e93aa2b385
Mohsin <mohsinshk4026@gmail.com>
2023-04-14 18:48:44 +0500

    create form for adding/removing society members

--- security/src/main/java/com/campusme/securitysvc/society/SocietyService.java
+++ security/src/main/java/com/campusme/securitysvc/society/SocietyService.java
@@ -2,20 +2,24 @@ package com.campusme.securitysvc.society;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.campusme.securitysvc.config.BearerAuthFeignConfig;
 
 import feign.Headers;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
@@ -39,13 +43,30 @@ class Tenure {
   List<Society> societies;
 }
 
+@Data
+class User {
+  private String code;
+}
+
+@Data
+class Member {
+  private User user;
+  private String role;
+  private Society society;
+}
+
+@Data
+@AllArgsConstructor
+class MemberDTO {
+  private String user;
+  private String role;
+  private String society;
+}
+
 /**
  * HTTP API client for the SOCIETY service
  */
-@FeignClient(
-  name="SOCIETY",
-  configuration=BearerAuthFeignConfig.class
-)
+@FeignClient(name = "SOCIETY", configuration = BearerAuthFeignConfig.class)
 interface FeignSocietyClient {
   @GetMapping("/societies")
   @CrossOrigin
@@ -54,6 +75,18 @@ interface FeignSocietyClient {
   @GetMapping("/societies/{code}")
   @CrossOrigin
   Society readOneSociety(@PathVariable String code);
+
+  @GetMapping("/memberships?society={code}")
+  @CrossOrigin
+  List<Member> readSocietyMembers(@PathVariable String code);
+
+  @DeleteMapping("/memberships")
+  @CrossOrigin
+  void deleteMember(@RequestBody MemberDTO dto);
+
+  @PostMapping("/memberships")
+  @CrossOrigin
+  void addMember(@RequestBody MemberDTO dto);
 
   @GetMapping("/tenures/{id}/societies")
   @CrossOrigin
@@ -83,6 +116,26 @@ public class SocietyService {
 
   public List<Society> findAllSocieties() {
     return client.readAllSocieties();
+  }
+
+  public Optional<Society> findByCode(String code) {
+    try {
+      return Optional.of(client.readOneSociety(code));
+    } catch (Exception e) {
+      return Optional.empty();
+    }
+  }
+
+  public List<Member> findSocietyMembers(String code) {
+    return client.readSocietyMembers(code);
+  }
+
+  public void deleteMembership(MemberDTO dto) {
+    client.deleteMember(dto);
+  }
+
+  public void addMembership(MemberDTO dto) {
+    client.addMember(dto);
   }
 
   public List<Tenure> findAllTenures() {
--- security/src/main/java/com/campusme/securitysvc/society/SocietyViewController.java
+++ security/src/main/java/com/campusme/securitysvc/society/SocietyViewController.java
@@ -1,6 +1,5 @@
 package com.campusme.securitysvc.society;
 
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,15 +10,25 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
 class SocietyCreateForm {
   Long tenure;
   SocietyCreateDTO society;
+}
+
+@Data
+@AllArgsConstructor
+class SocietyEditForm {
+  Society society;
+  List<Member> members;
 }
 
 @Controller
@@ -42,6 +51,31 @@ public class SocietyViewController {
 
     model.addAttribute("societies", societies);
     return "society";
+  }
+
+  @GetMapping("/society/edit/{code}")
+  public String edit(@PathVariable("code") String code, Model model) {
+    Society society = service.findByCode(code)
+        .orElseThrow(() -> new IllegalArgumentException("Invalid society code:" + code));
+
+    List<Member> members = service.findSocietyMembers(code);
+
+    model.addAttribute("form", new SocietyEditForm(society, members));
+    return "update-society";
+  }
+
+  @PostMapping("/society/member/delete")
+  public String deleteMember(MemberDTO dto, BindingResult result, Model model) {
+    service.deleteMembership(dto);
+
+    return "redirect:/society/edit/" + dto.getSociety();
+  }
+
+  @PostMapping("/society/member/add")
+  public String addMember(MemberDTO dto, BindingResult result, Model model) {
+    service.addMembership(dto);
+
+    return "redirect:/society/edit/" + dto.getSociety();
   }
 
   @PostMapping(path = "/society/create", consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
@@ -67,27 +101,28 @@ public class SocietyViewController {
   }
 
   // @PostMapping("/update/{id}")
-  // public String updateUser(@PathVariable("id") long id, RoleCreateDTO roleDTO, BindingResult result, Model model) {
-  //   Role role = new Role(
-  //       id,
-  //       roleDTO.getCode(),
-  //       roleDTO.getDescription(),
-  //       roleDTO.getName(),
-  //       roleDTO.getPermissionCodeList().stream()
-  //           .filter(perm -> perm.selected)
-  //           .map(perm -> permissionRepository.findByCode(perm.code))
-  //           .collect(Collectors.toSet()));
+  // public String updateUser(@PathVariable("id") long id, RoleCreateDTO roleDTO,
+  // BindingResult result, Model model) {
+  // Role role = new Role(
+  // id,
+  // roleDTO.getCode(),
+  // roleDTO.getDescription(),
+  // roleDTO.getName(),
+  // roleDTO.getPermissionCodeList().stream()
+  // .filter(perm -> perm.selected)
+  // .map(perm -> permissionRepository.findByCode(perm.code))
+  // .collect(Collectors.toSet()));
 
-  //   roleRepository.save(role);
+  // roleRepository.save(role);
 
-  //   return "redirect:/";
+  // return "redirect:/";
   // }
 
   // @GetMapping("/delete/{id}")
   // public String deleteRole(@PathVariable("id") long id, Model model) {
-  //   Role role = roleRepository.findById(id)
-  //       .orElseThrow(() -> new IllegalArgumentException("Invalid role Id:" + id));
-  //   roleRepository.delete(role);
-  //   return "redirect:/";
+  // Role role = roleRepository.findById(id)
+  // .orElseThrow(() -> new IllegalArgumentException("Invalid role Id:" + id));
+  // roleRepository.delete(role);
+  // return "redirect:/";
   // }
 }
