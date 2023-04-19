8a2d0290eff8eed288ececc2617737c21553a04b
roomiOwO <rooman.asifkhan@gmail.com>
2023-04-19 23:33:01 +0500

    added 4 logos, 1 icon no-bg assets

--- ui/packages/app/features/loading/screen.tsx
+++ ui/packages/app/features/loading/screen.tsx
@@ -4,15 +4,16 @@ export function LoadingScreen() {
   return (
     <View
       style={{
-        flex: 1, justifyContent: 'center', alignItems: 'center', padding: 16
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 16,
       }}
     >
-    <Image
-      style={{ width: 100, height: 100, borderRadius: 50 }}
-      source={{
-        uri: 'https://upload.wikimedia.org/wikipedia/commons/1/18/React_Native_Logo.png'
-      }}
-    />
+      <Image
+        style={{ width: 267, height: 150, borderRadius: 10 }}
+        source={require('../../assets/logoW.png')}
+      />
     </View>
   )
 }
