6e06454f603561268add2c544b90ba4a8ccc36f4
Mohsin <mohsinshk4026@gmail.com>
2023-04-24 17:40:41 +0500

    script for development in container

--- dev.sh
+++ dev.sh
@@ -1,35 +1,13 @@
-prefixwith() {
-	local prefix="$1"
-	shift
-	"$@" > >(sed "s/^/$prefix: /") 2> >(sed "s/^/$prefix (err): /" >&2)
-}
-
-gateway="mvn -f ./gateway clean spring-boot:run"
-society="mvn -f ./society clean spring-boot:run"
-security="mvn -f ./security clean spring-boot:run"
-team="mvn -f ./team clean spring-boot:run"
-
-run() {
-	eval ${!1}
-}
-
 if [ $# -eq 0 ]; then
 	echo "usage:"
-	echo "  ./dev.sh [service]"
-	echo "  ./dev.sh --tmux [service1] [service2] ..."
-	echo "services: gateway, society, security"
+	echo "  ./dev.sh [up|down] [service1] [service2] ..."
 	exit
 fi
 
-if [ "$1" == "--tmux" ]; then
-	tmux new-session -d
-	tmux rename dev
-	for svc in "${@:2}"; do
-		tmux new-window -n $svc
-		tmux send-keys "${!svc}" C-m
-	done
-	tmux a
-	exit
+params=()
+
+if [ $1 == "up" ]; then
+	params+=("--build")
 fi
 
-run $1
+docker-compose -f ./docker/docker-compose.yml -f ./docker/docker-compose.dev.yml $1 ${params[*]} ${@:2}
