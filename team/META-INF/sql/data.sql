f2940579701b590242e667d2060857f2ebf9b1dc
Mohsin <mohsinshk4026@gmail.com>
2023-05-07 02:04:19 +0500

    update seed data

--- team/META-INF/sql/data.sql
+++ team/META-INF/sql/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO app_user (code, first_name, last_name, email, photo) VALUES ('k191308@nu.edu.pk', 'K191308', 'Mohsin Shaikh', 'k191308@nu.edu.com', 'https://lh3.googleusercontent.com/a/AEdFTp7LUjuE26V7xi7ZqeTsUHCInWG-GSUCKDNkfCo-=s96-c');
-insert into team (code, name, society) VALUES ('test-spring-2023-pr', 'pr', 'test-spring-2023');
+insert into team (code, name, society) VALUES ('ExCo1-pr', 'pr', 'ExCo1');
 insert into member (team_id, user_id) VALUES (1, 1);
 
