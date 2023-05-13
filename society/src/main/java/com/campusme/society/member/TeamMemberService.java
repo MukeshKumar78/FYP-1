d7c0fda445c7d8e094c23160f74eeeacbf132d9d
Mohsin <mohsinshk4026@gmail.com>
2023-05-13 19:47:21 +0500

    add error handling in service to service request

--- society/src/main/java/com/campusme/society/member/TeamMemberService.java
+++ society/src/main/java/com/campusme/society/member/TeamMemberService.java
@@ -1,5 +1,6 @@
 package com.campusme.society.member;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,11 +8,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.campusme.society.config.BearerAuthFeignConfig;
 
+import feign.FeignException;
 import lombok.Data;
 
 @Data
@@ -50,6 +51,10 @@ public class TeamMemberService {
   }
 
   public List<TeamMember> getUserMemberships(String user) {
-    return client.readUserMemberships(user);
+    try {
+      return client.readUserMemberships(user);
+    } catch (FeignException.NotFound e) {
+      return new ArrayList<>();
+    }
   }
 }
